9e49a48adb9034499194e46d73b48182d8278488
Diego2250 <mor21146@uvg.edu.gt>
2022-02-25 13:36:40 -0600

    leer, escribir y crear archivo. Generacion de numeros random y escritura en archivo

--- Main.java
+++ Main.java
@@ -1,3 +1,22 @@
+import java.util.Scanner;
+import java.util.Random; 
 public class Main {
+   public static void main(String[] args) {
+    Scanner scan=new Scanner(System.in);
+    archivo arch=new archivo(); 
+    Random r1 = new Random();
+    int datos; 
+    String datoString; 
+    arch.crearchivo();
+    System.out.println("Ingrese el número de datos que desea utilizar");
+    datos=scan.nextInt();
+    int[] listanumeros=new int[datos]; 
+    for(int i=0; i<datos; i++){
+        listanumeros[i]=r1.nextInt(1000);
+        System.out.println(listanumeros[i]);
+        datoString=String.valueOf(listanumeros[i]);
+        arch.escribir(datoString);
+    }  
     
+   } 
 }
--- archivo.java
+++ archivo.java
@@ -1,3 +1,46 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import java.io.IOException;
 public class archivo {
-    
+
+  public archivo(){
+  }
+  public void crearchivo() {
+    try {
+      File myObj = new File("numeros.txt");
+      if (myObj.createNewFile()) {
+        System.out.println("Archivo creado: " + myObj.getName());
+      } else {
+      }
+    } catch (IOException e) {
+      e.printStackTrace();}
+    }
+
+  public void escribir(String dato){
+        try {
+        FileWriter myWriter = new FileWriter("numeros.txt", true);
+        myWriter.write(dato+"\n");
+        myWriter.close();
+        } catch (IOException e) {;
+        e.printStackTrace();
+    }
+    }
+        /*Metodo para leer el archivo
+      */
+  public void leer(){
+    try {
+      File myObj = new File("numeros.txt");
+      Scanner myReader = new Scanner(myObj);
+      while (myReader.hasNextLine()) {
+        String data = myReader.nextLine();
+        System.out.println(data);
+      }
+      myReader.close();
+    } catch (FileNotFoundException e) {
+      e.printStackTrace();
+    }
+  }
 }
+
