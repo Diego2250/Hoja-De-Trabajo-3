ffc7c49351838213da9a53850775299b78729ac8
Javier Alejandro Azurdia Arrecis <78101285+jazurdia@users.noreply.github.com>
2022-03-03 21:27:12 -0600

    JUnit 2/5
    radix and merge tests are now working.

--- sortTest.java
+++ sortTest.java
@@ -4,11 +4,14 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class sortTest {
 
+    sort t = new sort(new comparate());
+    int [] disordered_array = {9,8,7,6,5,4,3,2,1,10,11,12,13,100,1000};
+    // Para referencia:
+    //int [] ordered_array = {1,2,3,4,5,6,7,8,9,10,11,12,13,100,1000};
+
     @Test
     void bubbleSort() {
-        int [] disordered_array = {9,8,7,6,5,4,3,2,1,10,11,12,13,100,1000};
-        int [] ordered_array = {1,2,3,4,5,6,7,8,9,10,11,12,13,100,1000};
-        sort t = new sort(new comparate());
+
     }
 
     @Test
@@ -17,6 +20,7 @@ class sortTest {
 
     @Test
     void radix() {
+        assertEquals(4, t.radix(disordered_array)[3]);
     }
 
     @Test
@@ -25,6 +29,7 @@ class sortTest {
 
     @Test
     void merge() {
+        assertEquals(5, t.mergeSort(disordered_array)[4]);
     }
 
     @Test
